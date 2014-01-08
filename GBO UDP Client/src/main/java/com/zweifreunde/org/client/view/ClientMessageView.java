f9b90722540b85b7908f15c9ae218f0d9212879b
Ashish Shrestha <axhixh@users.github.com>
2014-01-08 18:11:40 +0000

    reformatted and using locale specific time formatting

--- GBO UDP Client/src/main/java/com/zweifreunde/org/client/view/ClientMessageView.java
+++ GBO UDP Client/src/main/java/com/zweifreunde/org/client/view/ClientMessageView.java
@@ -2,7 +2,6 @@ package com.zweifreunde.org.client.view;
 
 import java.awt.Insets;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.swing.JEditorPane;
@@ -12,52 +11,53 @@ import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 
 import com.zweifreunde.org.client.controller.NewMessagesListener;
+import java.text.DateFormat;
 
 public class ClientMessageView extends JScrollPane implements
-		NewMessagesListener {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1337L;
-	private JEditorPane textArea;
-	private HTMLEditorKit kit;
-	private HTMLDocument doc;
+        NewMessagesListener {
 
-	public ClientMessageView() {
-		this.textArea = new JEditorPane("text/html", null);
-		this.kit = new HTMLEditorKit();
-		this.doc = new HTMLDocument();
-		this.textArea.setEditorKit(kit);
-		this.textArea.setDocument(doc);
-		this.textArea.setEditable(false);
-		this.textArea.setMargin(new Insets(10, 10, 10, 10));
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1337L;
+    private JEditorPane textArea;
+    private HTMLEditorKit kit;
+    private HTMLDocument doc;
 
-		this.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+    public ClientMessageView() {
+        this.textArea = new JEditorPane("text/html", null);
+        this.kit = new HTMLEditorKit();
+        this.doc = new HTMLDocument();
+        this.textArea.setEditorKit(kit);
+        this.textArea.setDocument(doc);
+        this.textArea.setEditable(false);
+        this.textArea.setMargin(new Insets(10, 10, 10, 10));
 
-		this.setViewportView(this.textArea);
-	}
+        this.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
-	public void addMessage(String msg) {
-		try {
-			this.kit.insertHTML(doc, doc.getLength(), msg, 0, 0, null);
-		} catch (BadLocationException | IOException e) {
-		}
-	}
+        this.setViewportView(this.textArea);
+    }
 
-	@Override
-	public void newMessage(String msg) {
+    private void addMessage(String msg) {
+        try {
+            this.kit.insertHTML(doc, doc.getLength(), msg, 0, 0, null);
+        } catch (BadLocationException | IOException e) {
+        }
+    }
 
-		this.addMessage(String.format("%s %s", this.getHTMLTime(), msg));
+    @Override
+    public void newMessage(String msg) {
 
-	}
+        this.addMessage(String.format("%s %s", this.getHTMLTime(), msg));
 
-	public String getTime() {
-		SimpleDateFormat formatTime = new SimpleDateFormat("HH:mm:ss");
-		Date now = new Date();
-		return formatTime.format(now);
-	}
+    }
 
-	public String getHTMLTime() {
-		return "<a style='color: #999999'>&lt;" + getTime() + "&gt;</a>";
-	}
+    private String getTime() {
+        DateFormat df = DateFormat.getTimeInstance();
+        return df.format(new Date());
+    }
+
+    private String getHTMLTime() {
+        return "<a style='color: #999999'>&lt;" + getTime() + "&gt;</a>";
+    }
 }
