2b247f205875687a525ade681706775f0c323f17
Ashish Shrestha <axhixh@users.github.com>
2014-01-08 17:02:39 +0000

    adds code to announce user who just joined

--- GBO UDP Client/src/main/java/com/zweifreunde/org/client/Client.java
+++ GBO UDP Client/src/main/java/com/zweifreunde/org/client/Client.java
@@ -11,31 +11,24 @@ import com.zweifreunde.org.client.view.ClientWindow;
 import com.zweifreunde.org.multicast.UDPMulticastClient;
 
 public class Client {
-	public static void main(String[] args) throws NotBoundException,
-			IOException {
 
-		ClientModel clientModel = new ClientModel();
+    public static void main(String[] args) throws NotBoundException,
+            IOException {
 
-		ClientInputView civ = new ClientInputView();
-		ClientMessageView cmv = new ClientMessageView();
-		ClientWindow window = new ClientWindow(civ, cmv);
+        ClientModel clientModel = new ClientModel();
 
-		ClientInputController cIController = new ClientInputController(
-				clientModel, civ, window);
-		// Registriere ClientMessagesView f��r neue Nachrichten
-		clientModel.addNewMessageListener(cmv);
+        ClientInputView civ = new ClientInputView();
+        ClientMessageView cmv = new ClientMessageView();
+        ClientWindow window = new ClientWindow(civ, cmv);
 
-		// UDP standard
-		// DatagramSocket socket = new DatagramSocket();
-		// clientModel.addSendMessageListener(new UDPClientSender(1337,
-		// socket));
-		// UDPClientReceiver receiver = new UDPClientReceiver(clientModel,
-		// socket);
-		// receiver.startListening();
+        ClientInputController cIController = new ClientInputController(
+                clientModel, civ, window);
+        // Registriere ClientMessagesView f��r neue Nachrichten
+        clientModel.addNewMessageListener(cmv);
 
-		// UDP Multicast
-		UDPMulticastClient client = new UDPMulticastClient(clientModel, 1337);
-		client.startListening();
-
-	}
+        // UDP Multicast
+        UDPMulticastClient client = new UDPMulticastClient(clientModel, 1337);
+        client.startListening();
+        client.sendMessage(clientModel.getName() + " has joined.");
+    }
 }
