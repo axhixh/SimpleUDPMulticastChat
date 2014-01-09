06dccddfe813e134cd7237fe45e1ab53360168b0
Ashish Shrestha <axhixh@users.github.com>
2014-01-09 09:36:30 +0000

    fixes missing ; which was accidentally deleted (yikes!) and formatting to use spaces

--- GBO UDP Client/src/main/java/com/zweifreunde/org/multicast/UDPMulticastClient.java
+++ GBO UDP Client/src/main/java/com/zweifreunde/org/multicast/UDPMulticastClient.java
@@ -12,70 +12,71 @@ import com.zweifreunde.org.client.model.ClientModel;
 
 public class UDPMulticastClient implements SendMessageListener {
 
-	private MulticastSocket socket;
-	private InetAddress addr;
-	private int port;
-	private ClientModel model;
+    private MulticastSocket socket;
+    private InetAddress addr;
+    private int port;
+    private ClientModel model;
 
-	public UDPMulticastClient(ClientModel model, int port) throws IOException {
-		this.socket = new MulticastSocket(1337);
-		this.addr = InetAddress.getByName("238.254.254.254");
-		this.port = port;
-		this.socket.joinGroup(this.addr);
-		this.model = model;
-		this.model.addSendMessageListener(this);
-	}
+    public UDPMulticastClient(ClientModel model, int port) throws IOException {
+        this.socket = new MulticastSocket(1337);
+        this.addr = InetAddress.getByName("238.254.254.254");
+        this.port = port;
+        this.socket.joinGroup(this.addr);
+        this.model = model;
+        this.model.addSendMessageListener(this);
+    }
 
-	@Override
-	public void sendMessage(String msg) {
-		DatagramPacket packet = new DatagramPacket(msg.getBytes(),
-				msg.getBytes().length, this.addr, this.port);
-		try {
-			this.socket.send(packet);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void sendMessage(String msg) {
+        DatagramPacket packet = new DatagramPacket(msg.getBytes(),
+                msg.getBytes().length, this.addr, this.port);
+        try {
+            this.socket.send(packet);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	public void startListening() {
-		Thread thread = new Thread(new Runnable() {
+    public void startListening() {
+        Thread thread = new Thread(new Runnable() {
 
-			@Override
-			public void run() {
-				while (true) {
-					byte[] b = new byte[2048];
-					DatagramPacket read = new DatagramPacket(b, b.length);
-					try {
-						socket.receive(read);
-					} catch (IOException e) {
-						System.out.println("Error receiving.");
-					}
-					String msg = new String(read.getData());
-					SwingUtilities.invokeLater(new MsgRunnable(model, msg));
-				}
+            @Override
+            public void run() {
+                while (true) {
+                    byte[] b = new byte[2048];
+                    DatagramPacket read = new DatagramPacket(b, b.length);
+                    try {
+                        socket.receive(read);
+                    } catch (IOException e) {
+                        System.out.println("Error receiving.");
+                    }
+                    String msg = new String(read.getData());
+                    SwingUtilities.invokeLater(new MsgRunnable(model, msg));
+                }
 
-			}
-		});
-        thread.setDaemon(true)
-		thread.start();
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
 
-	}
+    }
 
-	class MsgRunnable implements Runnable {
-		private String msg;
-		private ClientModel model;
+    class MsgRunnable implements Runnable {
 
-		public MsgRunnable(ClientModel model, String msg) {
-			this.model = model;
-			this.msg = msg;
-		}
+        private String msg;
+        private ClientModel model;
 
-		@Override
-		public void run() {
-			this.model.newMessage(this.msg.trim());
+        public MsgRunnable(ClientModel model, String msg) {
+            this.model = model;
+            this.msg = msg;
+        }
 
-		}
+        @Override
+        public void run() {
+            this.model.newMessage(this.msg.trim());
 
-	}
+        }
+
+    }
 }
