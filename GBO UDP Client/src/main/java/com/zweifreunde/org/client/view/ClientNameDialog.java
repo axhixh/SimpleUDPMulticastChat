f48344dd8ca37db4e611896215eab2ca7868b8d7
Ashish Shrestha <axhixh@users.github.com>
2014-01-09 12:43:46 +0000

    makes sure name is not empty

--- GBO UDP Client/src/main/java/com/zweifreunde/org/client/view/ClientNameDialog.java
+++ GBO UDP Client/src/main/java/com/zweifreunde/org/client/view/ClientNameDialog.java
@@ -1,28 +1,25 @@
 package com.zweifreunde.org.client.view;
 
 import java.util.ResourceBundle;
-import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 
-public class ClientNameDialog extends JDialog {
-	/**
-	 * 
-	 */
+public class ClientNameDialog {
+
     private static final long serialVersionUID = 1337L;
     private static final Object QUESTION = ResourceBundle.getBundle("com/zweifreunde/etc/Messages").getString("What is your name?");
     private static final String TITLE = ResourceBundle.getBundle("com/zweifreunde/etc/Messages").getString("LOGIN");
-	private ClientWindow window;
+    private ClientWindow window;
 
-	public ClientNameDialog(ClientWindow window) {
-		this.window = window;
-	}
+    public ClientNameDialog(ClientWindow window) {
+        this.window = window;
+    }
 
-	public String askForName() {
-		String name = null;
-		while (name == null) {
-			name = JOptionPane.showInputDialog(this.window, QUESTION, TITLE,
-					JOptionPane.PLAIN_MESSAGE);
-		}
-		return name;
-	}
+    public String askForName() {
+        String name = null;
+        while (name == null || name.trim().length() == 0) {
+            name = JOptionPane.showInputDialog(this.window, QUESTION, TITLE,
+                    JOptionPane.PLAIN_MESSAGE);
+        }
+        return name;
+    }
 }
